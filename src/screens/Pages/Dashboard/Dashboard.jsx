b66bc711db558f1b4e509a4721c95935341b78c1
av7388605503 <av84002499@gmail.com>
2025-02-03 23:15:45 +0530

    update

--- src/screens/Pages/Dashboard/Dashboard.jsx
+++ src/screens/Pages/Dashboard/Dashboard.jsx
@@ -3,101 +3,104 @@ import "./Dashboard.css";
 
 const Dashboard = () => {
     return (
-        <div className="card4">
-            <h2 className="card-header" style={{ marginLeft: "40px" }}>Match Overview</h2>
+        <>
+            <div className="card4">
+                <h2 className="card-header" style={{ marginLeft: "40px" }}>Match Overview</h2>
 
-            <hr />
+                <hr />
 
-            <div className="card-body">
+                <div className="card-body">
 
-                <div class="card-group">
-                    <div class="card5">
-                        <div class="card-content">
-                            <h2>Match 1</h2>
-                            <p>Team A vs Team B</p>
-                            <p>03:30 PM, Live</p>
+                    <div class="card-group">
+                        <div class="card5">
+                            <div class="card-content">
+                                <h2>Match 1</h2>
+                                <p>Team A vs Team B</p>
+                                <p>03:30 PM, Live</p>
+                            </div>
+                            <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
                         </div>
-                        <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
-                    </div>
-                    <div class="card5">
-                        <div class="card-content">
-                            <h2>Match 1</h2>
-                            <p>Team A vs Team B</p>
-                            <p>03:30 PM, Live</p>
+                        <div class="card5">
+                            <div class="card-content">
+                                <h2>Match 1</h2>
+                                <p>Team A vs Team B</p>
+                                <p>03:30 PM, Live</p>
+                            </div>
+                            <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
                         </div>
-                        <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
-                    </div>
-                    <div class="card5">
-                        <div class="card-content">
-                            <h2>Match 1</h2>
-                            <p>Team A vs Team B</p>
-                            <p>03:30 PM, Live</p>
+                        <div class="card5">
+                            <div class="card-content">
+                                <h2>Match 1</h2>
+                                <p>Team A vs Team B</p>
+                                <p>03:30 PM, Live</p>
+                            </div>
+                            <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
                         </div>
-                        <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
+
                     </div>
+
+                    <div class="card-group">
+                        <div class="card5">
+                            <div class="card-content">
+                                <h2>Match 1</h2>
+                                <p>Team A vs Team B</p>
+                                <p>03:30 PM, Live</p>
+                            </div>
+                            <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
+                        </div>
+                        <div class="card5">
+                            <div class="card-content">
+                                <h2>Match 1</h2>
+                                <p>Team A vs Team B</p>
+                                <p>03:30 PM, Live</p>
+                            </div>
+                            <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
+                        </div>
+                        <div class="card5">
+                            <div class="card-content">
+                                <h2>Match 1</h2>
+                                <p>Team A vs Team B</p>
+                                <p>03:30 PM, Live</p>
+                            </div>
+                            <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
+                        </div>
+                    </div>
+
+                    <div class="card-group ">
+                        <div class="card5">
+                            <div class="card-content">
+                                <h2>Match 1</h2>
+                                <p>Team A vs Team B</p>
+                                <p>03:30 PM, Live</p>
+
+                            </div>
+                            <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
+                        </div>
+                        <div class="card5">
+                            <div class="card-content">
+                                <h2>Match 1</h2>
+                                <p>Team A vs Team B</p>
+                                <p>03:30 PM, Live</p>
+                            </div>
+                            <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
+                        </div>
+                        <div class="card5">
+                            <div class="card-content">
+                                <h2>Match 1</h2>
+                                <p>Team A vs Team B</p>
+                                <p>03:30 PM, Live</p>
+                            </div>
+                            <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
+                        </div>
+                    </div>
+
 
                 </div>
-
-                <div class="card-group">
-                    <div class="card5">
-                        <div class="card-content">
-                            <h2>Match 1</h2>
-                            <p>Team A vs Team B</p>
-                            <p>03:30 PM, Live</p>
-                        </div>
-                        <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
-                    </div>
-                    <div class="card5">
-                        <div class="card-content">
-                            <h2>Match 1</h2>
-                            <p>Team A vs Team B</p>
-                            <p>03:30 PM, Live</p>
-                        </div>
-                        <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
-                    </div>
-                    <div class="card5">
-                        <div class="card-content">
-                            <h2>Match 1</h2>
-                            <p>Team A vs Team B</p>
-                            <p>03:30 PM, Live</p>
-                        </div>
-                        <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
-                    </div>
-                </div>
-
-                <div class="card-group ">
-                    <div class="card5">
-                        <div class="card-content">
-                            <h2>Match 1</h2>
-                            <p>Team A vs Team B</p>
-                            <p>03:30 PM, Live</p>
-
-                        </div>
-                        <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
-                    </div>
-                    <div class="card5">
-                        <div class="card-content">
-                            <h2>Match 1</h2>
-                            <p>Team A vs Team B</p>
-                            <p>03:30 PM, Live</p>
-                        </div>
-                        <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
-                    </div>
-                    <div class="card5">
-                        <div class="card-content">
-                            <h2>Match 1</h2>
-                            <p>Team A vs Team B</p>
-                            <p>03:30 PM, Live</p>
-                        </div>
-                        <img src="../assets/Image135.jpg" alt="fotbal" class="card-img-top" />
-                    </div>
-                </div>
-
             </div>
 
             <div className="card6">
                 <h2 className="card-header" style={{ marginLeft: "30px" }}>User Dashboard</h2>
-                    <hr />
+                <hr />
                 <div className="card-body">
                     <div class="card-group ">
 
@@ -125,7 +128,7 @@ const Dashboard = () => {
                                     <p className="wallet-title">Contests Summary</p>
                                     <h1 className="wallet-amount">18%</h1>
                                     <div className="arrow-up">75%</div>
-                                    
+
                                 </div>
                                 <div className="wallet-chart">
                                     <div className="bar tall"></div>
@@ -161,18 +164,17 @@ const Dashboard = () => {
             </div>
 
             <div class="container3">
-        <div class="section">
-            <div class="content">
-                <h11>Join New Contests</h11>
-                <p1>Participate in exciting new contests and showcase your skills against fellow sports enthusiasts!</p1>
-                <button>Join for free</button>
+                <div class="section">
+                    <div class="content">
+                        <h11>Join New Contests</h11>
+                        <p1>Participate in exciting new contests and showcase your skills against fellow sports enthusiasts!</p1>
+                        <button>Join for free</button>
+                    </div>
+                    <div class="image"></div>
+                </div>
             </div>
-            <div class="image"></div>
-        </div>
-    </div>
 
-
-        </div>
+        </>
     );
 };
 
--- src/screens/Pages/Teammanagement/Teammanagement.jsx
+++ src/screens/Pages/Teammanagement/Teammanagement.jsx
@@ -4,30 +4,38 @@ import "./Teammanagement.css"
 const Teammanagement = () => {
   return (
     <>
-      <div>
-        <div className='contain4'>
-          <div class="row">
-            <div class="col-sm-6 mb-3 mb-sm-0">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                  <a href="!" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
+     <div class="container4">
+        <h2>Upcoming & Live Matches</h2>
+        <hr />
+        <div class="matches-grid">
+            <div class="match-card">
+                <p>Team A vs Team B</p>
+                <p>12:00 PM, <span class="live">Live</span></p>
             </div>
-            <div class="col-sm-6">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                  <a href="!" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
+           
+            <div class="match-card">
+                <p>Team E vs Team F</p>
+                <p>4:00 PM, Upcoming</p>
             </div>
-          </div>
+            <div class="match-card">
+                <p>Team G vs Team H</p>
+                <p>6:00 PM, Upcoming</p>
+            </div>
+            <div class="match-card">
+                <p>Team G vs Team H</p>
+                <p>6:00 PM, Upcoming</p>
+            </div>
+            <div class="match-card">
+                <p>Team G vs Team H</p>
+                <p>6:00 PM, Upcoming</p>
+            </div>
+            <div class="match-card">
+                <p>Team G vs Team H</p>
+                <p>6:00 PM, Upcoming</p>
+            </div>
         </div>
-      </div>
+    </div>
+
     </>
   )
 }
